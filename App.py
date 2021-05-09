46b1e7ffe4b65e8bf0b8169792e44dbab2f99df2
GOURISANKAR ACHARYA <74904092+Acharya0702@users.noreply.github.com>
2021-05-09 23:16:00 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,30 @@
+import flask
+from flask import Flask
+import sklearn.externals
+import joblib
+from flask import render_template, request
+import numpy as np
+app = Flask(__name__)
+model = joblib.load('model.pkl')
+@app.route("/")
+def home():
+   return render_template('Iris.html')
+@app.route('/predict', methods=['POST'])
+def make_prediction():
+    if request.method=='POST':
+        f1 = request.form['f1']
+        f2 = request.form['f2']
+        f3 = request.form['f3']
+        f4 = request.form['f4']
+        feature_array = [f1,f2,f3,f4]
+        feature = np.asarray(feature_array, dtype='float64').reshape(1,-1)
+        prediction = model.predict(feature)
+        if prediction == 0:
+            prediction='Iris-setosa'
+        elif prediction == 1:
+            prediction='Iris-versicolor'
+        else:
+            prediction='Iris-virginica'
+        return render_template('Iris.html', prediction='{}'.format(prediction))
+if __name__ == '__main__':
+    app.run(debug=True)
